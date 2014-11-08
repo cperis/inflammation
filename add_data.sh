75c833faeeef81d6ad38eae5f16aebd32b79dea6
Charith Peris <cperis@cfa.harvard.edu>
2014-11-08 15:49:00 -0500

    Adding add_data.sh again

--- add_data.sh
+++ add_data.sh
@@ -1,2 +1,7 @@
 type=`python assign_drug.py $2`
-echo cp $2 data/$2/$1/$1-$type.dat
+dest=`echo data/$2/$1/$1-$type.dat`
+cp $2 $dest
+git add $dest
+MSG="Add file $dest"
+git commit -m $MSG
+echo "New file added to the report: $dest"
